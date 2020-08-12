d32e6ee45f75ffaf1af1f2fea8ae5ed93112d0c2
Haibo Huang <hhb@google.com>
2020-08-12 13:52:04 -0700

    Upgrade rust/crates/protobuf to 2.17.0
    * Android.bp was regenerated. New rustc warnings were found.
    * Keep local change in src/lib.rs to include out/version.rs.
    * Update out/version.rs to the new version.
    
    Change-Id: I9c2f7d412dcd42fe3769505629e05ab949300fe6
    Test: make

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! Library to read and write protocol buffers data.
 
 #![deny(missing_docs)]
-#![deny(intra_doc_link_resolution_failure)]
+#![deny(broken_intra_doc_links)]
 // Because we need compat with Rust 1.26
 #![allow(bare_trait_objects)]
 
@@ -12,36 +12,36 @@ extern crate serde;
 #[macro_use]
 #[cfg(feature = "with-serde")]
 extern crate serde_derive;
-pub use cached_size::CachedSize;
+pub use crate::cached_size::CachedSize;
 #[cfg(feature = "bytes")]
-pub use chars::Chars;
-pub use clear::Clear;
-pub use core::parse_from_bytes;
+pub use crate::chars::Chars;
+pub use crate::clear::Clear;
+pub use crate::enums::ProtobufEnum;
+pub use crate::error::ProtobufError;
+pub use crate::error::ProtobufResult;
+pub use crate::message::parse_from_bytes;
 #[cfg(feature = "bytes")]
-pub use core::parse_from_carllerche_bytes;
-pub use core::parse_from_reader;
+pub use crate::message::parse_from_carllerche_bytes;
+pub use crate::message::parse_from_reader;
 #[allow(deprecated)]
-pub use core::parse_length_delimited_from;
+pub use crate::message::parse_length_delimited_from;
 #[allow(deprecated)]
-pub use core::parse_length_delimited_from_bytes;
+pub use crate::message::parse_length_delimited_from_bytes;
 #[allow(deprecated)]
-pub use core::parse_length_delimited_from_reader;
-pub use core::Message;
-pub use enums::ProtobufEnum;
-pub use error::ProtobufError;
-pub use error::ProtobufResult;
-pub use repeated::RepeatedField;
-pub use singular::SingularField;
-pub use singular::SingularPtrField;
-pub use stream::wire_format;
-pub use stream::CodedInputStream;
-pub use stream::CodedOutputStream;
-pub use unknown::UnknownFields;
-pub use unknown::UnknownFieldsIter;
-pub use unknown::UnknownValue;
-pub use unknown::UnknownValueRef;
-pub use unknown::UnknownValues;
-pub use unknown::UnknownValuesIter;
+pub use crate::message::parse_length_delimited_from_reader;
+pub use crate::message::Message;
+pub use crate::repeated::RepeatedField;
+pub use crate::singular::SingularField;
+pub use crate::singular::SingularPtrField;
+pub use crate::stream::wire_format;
+pub use crate::stream::CodedInputStream;
+pub use crate::stream::CodedOutputStream;
+pub use crate::unknown::UnknownFields;
+pub use crate::unknown::UnknownFieldsIter;
+pub use crate::unknown::UnknownValue;
+pub use crate::unknown::UnknownValueRef;
+pub use crate::unknown::UnknownValues;
+pub use crate::unknown::UnknownValuesIter;
 
 // generated
 pub mod descriptor;
@@ -50,12 +50,13 @@ pub mod rustproto;
 
 mod clear;
 pub mod compiler_plugin;
-mod core;
 mod enums;
 pub mod error;
 pub mod ext;
+pub mod json;
 pub mod lazy;
 mod lazy_v2;
+mod message;
 pub mod reflect;
 mod repeated;
 pub mod rt;
@@ -87,34 +88,6 @@ mod misc;
 
 mod buf_read_iter;
 
-// so `use protobuf::*` could work in mod descriptor and well_known_types
-mod protobuf {
-    pub use cached_size::CachedSize;
-    pub use clear::Clear;
-    pub use core::*;
-    pub use descriptor;
-    pub use descriptorx;
-    pub use enums::ProtobufEnum;
-    pub use error::*;
-    pub use ext;
-    pub use lazy;
-    pub use reflect;
-    pub use repeated::RepeatedField;
-    pub use rt;
-    pub use singular::SingularField;
-    pub use singular::SingularPtrField;
-    pub use stream::*;
-    pub use text_format;
-    pub use types;
-    pub use unknown::UnknownFields;
-    pub use unknown::UnknownFieldsIter;
-    pub use unknown::UnknownValue;
-    pub use unknown::UnknownValueRef;
-    pub use unknown::UnknownValues;
-    pub use unknown::UnknownValuesIter;
-    pub use well_known_types;
-}
-
 /// This symbol is in generated `version.rs`, include here for IDE
 #[cfg(never)]
 pub const VERSION: &str = "";
@@ -122,4 +95,4 @@ pub const VERSION: &str = "";
 #[cfg(never)]
 #[doc(hidden)]
 pub const VERSION_IDENT: &str = "";
-include!("../out/version.rs");
+include!("../out/version.rs");  // ANDROID
--- src/repeated.rs
+++ src/repeated.rs
@@ -16,7 +16,7 @@ use std::ops::IndexMut;
 use std::slice;
 use std::vec;
 
-use clear::Clear;
+use crate::clear::Clear;
 
 /// Wrapper around vector to avoid deallocations on clear.
 pub struct RepeatedField<T> {
@@ -205,6 +205,31 @@ impl<T> RepeatedField<T> {
         assert!(index < self.len);
         self.len -= 1;
         self.vec.remove(index)
+    }
+
+    /// Retains only the elements specified by the predicate.
+    ///
+    /// In other words, remove all elements `e` such that `f(&e)` returns `false`.
+    /// This method operates in place, visiting each element exactly once in the
+    /// original order, and preserves the order of the retained elements.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use protobuf::RepeatedField;
+    ///
+    /// let mut vec = RepeatedField::from(vec![1, 2, 3, 4]);
+    /// vec.retain(|&x| x % 2 == 0);
+    /// assert_eq!(vec, RepeatedField::from(vec![2, 4]));
+    /// ```
+    pub fn retain<F>(&mut self, f: F)
+    where
+        F: FnMut(&T) -> bool,
+    {
+        // suboptimal
+        self.vec.truncate(self.len);
+        self.vec.retain(f);
+        self.len = self.vec.len();
     }
 
     /// Truncate at specified length.
