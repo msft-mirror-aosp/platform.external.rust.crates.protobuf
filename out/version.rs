4bf8b4675749f0f2cfc0a7e90398b0834635cc28
Haibo Huang <hhb@google.com>
2020-11-24 20:53:50 -0800

    Upgrade rust/crates/protobuf to 2.18.1
    Test: make
    Change-Id: I5b915568dd8c388d36820424b8c7986dfd33af3b

--- out/version.rs
+++ out/version.rs
@@ -1,7 +1,7 @@
 /// protobuf crate version
-pub const VERSION: &'static str = "2.18.0";
+pub const VERSION: &'static str = "2.18.1";
 /// This symbol is used by codegen
 #[doc(hidden)]
-pub const VERSION_IDENT: &'static str = "VERSION_2_18_0";
+pub const VERSION_IDENT: &'static str = "VERSION_2_18_1";
 /// This symbol can be referenced to assert that proper version of crate is used
-pub const VERSION_2_18_0: () = ();
+pub const VERSION_2_18_1: () = ();
